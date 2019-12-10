344cf686657de684562f6224153cdf9b675fb264
Tarric Sookdeo <tarricsookdeo@outlook.com>
2019-12-09 19:51:52 -0500

    Will add logic for /games/index.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+    get '/games/show' do
+        if logged_in?
+            @games = []
+            all_games = Game.all
+
+            all_games.each do |g|
+                if g.user_id == current_user.id
+                    @games << g
+                end
+            end
+
+            erb :'games/index'
+        else
+            redirect '/user/login'
+        end
+    end
+
 end
