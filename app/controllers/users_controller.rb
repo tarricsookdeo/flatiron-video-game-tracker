f353f1b8b07745d2ff5d085a9dcd5d61e98d8136
Tarric Sookdeo <tarricsookdeo@outlook.com>
2019-12-09 19:20:38 -0500

    Will create the /signup post request logic.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,17 @@ class UsersController < ApplicationController
     get '/user/signup' do
         erb :'users/signup'
     end
-    
+
+    post '/signup' do
+        @u = User.new
+        @u.username = params[:username]
+        @u.password = params[:password]
+
+        if @u.save
+            redirect '/user/login'
+        else
+            redirect '/user/signup'
+        end
+    end
+
 end
