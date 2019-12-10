998f58f59bb16867c8a6e26fa2be2a2b17ffffb4
Tarric Sookdeo <tarricsookdeo@outlook.com>
2019-12-09 19:42:21 -0500

    Will update schema.rb to add games model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191210000134) do
+ActiveRecord::Schema.define(version: 20191210003903) do
+
+  create_table "games", force: :cascade do |t|
+    t.string  "title"
+    t.string  "publisher"
+    t.string  "rating"
+    t.integer "year_released"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
