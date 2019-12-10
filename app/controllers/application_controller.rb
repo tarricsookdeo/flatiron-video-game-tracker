1454543132756a6456900d8bd0d9c64721098a1d
Tarric Sookdeo <tarricsookdeo@outlook.com>
2019-12-09 19:20:10 -0500

    Will enable sessions, and set the session_secret.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "flatiron_video_game_tracker"
   end
 
   get "/" do
