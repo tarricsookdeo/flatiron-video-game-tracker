b1841bd63008a18011189ef473a5daf1a3be65ce
Tarric Sookdeo <tarricsookdeo@outlook.com>
2019-12-10 19:10:10 -0500

    Will add post logic to games.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,19 +2,30 @@ class GamesController < ApplicationController
 
     get '/games' do
         if logged_in?
-            @games = []
-            all_games = Game.all
-
-            all_games.each do |g|
-                if g.user_id == current_user.id
-                    @games << g
-                end
-            end
+            @games = current_user.games
 
             erb :'games/index'
         else
             redirect '/login'
         end
+    end
+
+    get '/games/new' do
+        erb :'games/new'
+    end
+
+    post '/games' do
+        @g = current_user.games.build(title: params[:title], publisher: params[:publisher], year_released: params[:year_released], rating: params[:rating])
+
+        if @g.save
+            redirect '/games'
+        else
+            redirect '/games/new'
+        end
+    end
+
+    delete '/games/:id' do
+        binding.pry
     end
 
 
